c918afb2c210aa5d6fc30a4c0421eaeb7c27f4a4
Ori Erez <orierez@Oris-MacBook-Air.local>
2025-11-30 15:12:30 -0500

    Got the random Rock Paper Scissors function working in the console.

---
+++ rockpaperscissors.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    const ans = Math.random();
+    if (ans >= 0 && and < 0.33){
+        return "Rock";
+    }
+    if (ans >= 0.33 && and < 0.66){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+} 
